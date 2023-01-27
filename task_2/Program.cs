717f76d983fc1b970b1ad130a5f1478c34c3c4da
OndreAbro <denbrysommemanen@gmail.com>
2023-01-27 17:43:38 +0700

    push homework

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1;
+
+if (number2 >= result)
+{
+    result = number2;
+}
+
+System.Console.WriteLine($"max = {result}");
---
+++ task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0)
+{
+    Console.Write("Чётные числа от -1 до N: ");
+    for (int i = 2; i <= -N; i += 2)
+        Console.Write("-" + i + " ");
+}
+else
+{
+    Console.Write("Чётные числа от 1 до N: ");
+    for (int i = 2; i <= N; i += 2)
+    {
+        Console.Write(i + " ");
+    }
+}
